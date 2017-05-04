e8c2633edff4fe8de090c109f1be61d975571837
Juan Ignacio <Juan Ignacio@JuanIgnacio-PC>
2017-05-04 02:38:42 -0300

    eventos handle y weas; variable global se cambia en run o no pesca la root click

--- AutomataApp/src/MainWindowController.java
+++ AutomataApp/src/MainWindowController.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
@@ -41,6 +48,13 @@ public class MainWindowController extends Application{
     
     private GridPane rootLayout;
     
+    private boolean isStartNode = false;
+    private boolean isEndNode = false;
+    private boolean isStandNode = false;
+    private boolean isTransition = false;
+    
+    private BufferedImage img;
+    
     
     public static void main(String[] args)
     {
@@ -62,41 +76,72 @@ public class MainWindowController extends Application{
         
             primaryStage.setTitle("Main Application");
             primaryStage.show();
-//            this.startNode.setOnAction(this::handleButtonAction);
-//            this.startNode.setOnAction((e) -> handleBottonAction(e));
-            /*this.startNode.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
-                
-                System.out.println("holi");
+            try{
+            img = ImageIO.read(new File("src/imagenes/dc6.jpg"));            
             }
-        });*/
+            catch(IOException e)
+            {
+            }
+            
+            rootLayout.setOnMouseClicked(new EventHandler<MouseEvent>() { 
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("apretaaa!!!!!!!!!!");
+                System.out.println("isHANDLE: " + isIsStartNode());
+//                System.out.println("getCursor: " + getStartNode().getCursor().toString());
+                if(isIsStandNode())
+                {
+                    //agregar nodo inicial, desactivar booleano y boton
+                    System.out.println("apretaaa1");
+                }
+                else if(isEndNode)
+                {
+                    //agregar nodo, desactivar booleano
+                    System.out.println("apreta222");
+                }
+                else if(isStandNode)
+                {
+                    //agregar nodo, desactivar booleano
+                }
+                else if(isTransition){
+
+                }
+            }
+        });
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
     }
+    
     @FXML
     public void handleButtonAction(ActionEvent event) 
     {
         if(event.getSource() == this.startNode)
         {
+            System.out.println("is: " + this.isStartNode);
             System.out.println("holi1");
+            this.setIsStartNode(true);
+            System.out.println("is: " + this.isStartNode);
+            startNode.setCursor(Cursor.WAIT);
         }
         
         if(event.getSource() == this.endNode)
         {
             System.out.println("holi2");
+            this.isEndNode = true;
         }
         
         if(event.getSource() == this.standNode)
         {
             System.out.println("holi3");
+            this.isStandNode = true;
         }
         
         if(event.getSource() == this.transition)
         {
-            
+            this.isTransition = true;
         }
     }
     /*@Override
@@ -108,6 +153,112 @@ public class MainWindowController extends Application{
         this.startNode.setOnAction(this::handleButtonAction);
         this.standNode.setOnAction(this::handleButtonAction);
     }*/
+
+    /*@Override
+    public void mouseClicked(MouseEvent e) {
+        
+        if(isStartNode)
+        {
+            //agregar nodo inicial, desactivar booleano y boton
+            System.out.println("apretaaa1");
+        }
+        else if(isEndNode)
+        {
+            //agregar nodo, desactivar booleano
+            System.out.println("apreta222");
+        }
+        else if(isStandNode)
+        {
+            //agregar nodo, desactivar booleano
+        }
+        else if(isTransition){
+            
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }*/
+
+    public boolean isIsStartNode() {
+        return isStartNode;
+    }
+
+    public void setIsStartNode(boolean isStartNode) {
+        this.isStartNode = isStartNode;
+    }
+
+    public boolean isIsEndNode() {
+        return isEndNode;
+    }
+
+    public void setIsEndNode(boolean isEndNode) {
+        this.isEndNode = isEndNode;
+    }
+
+    public boolean isIsStandNode() {
+        return isStandNode;
+    }
+
+    public void setIsStandNode(boolean isStandNode) {
+        this.isStandNode = isStandNode;
+    }
+
+    public boolean isIsTransition() {
+        return isTransition;
+    }
+
+    public void setIsTransition(boolean isTransition) {
+        this.isTransition = isTransition;
+    }
+
+    public Button getStartNode() {
+        return startNode;
+    }
+
+    public void setStartNode(Button startNode) {
+        this.startNode = startNode;
+    }
+
+    public Button getEndNode() {
+        return endNode;
+    }
+
+    public void setEndNode(Button endNode) {
+        this.endNode = endNode;
+    }
+
+    public Button getStandNode() {
+        return standNode;
+    }
+
+    public void setStandNode(Button standNode) {
+        this.standNode = standNode;
+    }
+
+    public Button getTransition() {
+        return transition;
+    }
+
+    public void setTransition(Button transition) {
+        this.transition = transition;
+    }
     
         
     
